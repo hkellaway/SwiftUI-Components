ae020dee098b4d8ea6dd6ed9da46f8ffd7e01cbb
Harlan Kellaway <hkellaway@users.noreply.github.com>
2020-11-14 20:54:34 -0500

    Create demo Xcode project

---
+++ SwiftUIComponentsDemo/SwiftUIComponentsDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIComponentsDemo
+//
+//  Created by Harlan Kellaway on 11/14/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
